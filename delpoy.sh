09ab5a45749a66c69c40183ec82eae9063d82502
xue-yuan <g00se.9527@gmail.com>
2020-05-06 11:09:20 +0800

    development

---
+++ delpoy.sh
@@ -0,0 +1,14 @@
+# bin/bash
+
+npm run build
+cp CNAME /dist
+git init
+git add .
+git commit -m "development"
+git remote add origin https://github.com/xue-yuan/xue-yuan.github.io.git
+git push -u origin master
+
+cd dist/
+git add .
+git commit -m "production"
+git push -f https://github.com/xue-yuan/xue-yuan.github.io.git master:source
