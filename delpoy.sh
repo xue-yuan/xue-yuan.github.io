307f834c69a6989083b5b664575cbb7f51ab7f27
xue-yuan <g00se.9527@gmail.com>
2020-05-06 11:17:51 +0800

    development

--- delpoy.sh
+++ delpoy.sh
@@ -8,8 +8,9 @@ pwd
 git init
 git add .
 git commit -m "production"
-git remote add origin https://github.com/xue-yuan/xue-yuan.github.io.git
-git push -u origin master
+git push -f https://github.com/xue-yuan/xue-yuan.github.io.git
+# git remote add origin https://github.com/xue-yuan/xue-yuan.github.io.git
+# git push -u origin master
 
 cd ../
 git init
