9d3be786e1f44987db5e279b8953fbff051b5abe
xue-yuan <g00se.9527@gmail.com>
2020-05-06 11:12:47 +0800

    development

--- delpoy.sh
+++ delpoy.sh
@@ -5,10 +5,11 @@ cp CNAME ./dist
 git init
 git add .
 git commit -m "development"
-git remote add origin https://github.com/xue-yuan/xue-yuan.github.io.git
-git push -u origin master
+git push -f https://github.com/xue-yuan/xue-yuan.github.io.git master:source
 
 cd dist/
+git init
 git add .
 git commit -m "production"
-git push -f https://github.com/xue-yuan/xue-yuan.github.io.git master:source
+git remote add origin https://github.com/xue-yuan/xue-yuan.github.io.git
+git push -u origin master
